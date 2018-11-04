6adc44548984d9f8ca64fc8b0c9d36f121a55a3a
Sajid305 <mdshajid305@gmail.com>
2018-11-05 03:12:07 +0600

    php basic practice code

---
+++ Source Code/index.php
@@ -0,0 +1,46 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>First php</title>
+</head>
+<body>
+    <?php
+    
+    // echo("<h1>Hello World</h1>");
+    // echo("<hr>");
+    // echo("<P>This is my php website</p>");
+    // $shajid = 'shjaid rayhan';
+    // echo strlen ($shajid); len method
+    // $name = 'shajid';
+    // $name[0] = 'S';
+    // echo strtoupper($name); upperCase method
+    // echo strtolower($name); lowerCase method
+    // echo $name[0]; //index method
+
+    // $string = 'shajid ryhan';
+    // echo str_replace("shajid","monti",$string); string replace method
+    // echo substr($string,9);// grabing a value from string
+
+    // working with numbers
+
+    // $number1 = 6;
+    // $number2 = 12;               // we can all short of math oparation like this
+    // $totalNumber = $number1+$number2;
+    // echo $totalNumber;
+    // echo 5*10;
+    // echo 10%3;
+    // echo 4 + 5 * 10;
+    // echo (4 + 5) * 10;
+    // $number1 = 6;
+    // $number1++;
+    // echo $number1;
+    
+    ?>
+</body>
+</html>
+
